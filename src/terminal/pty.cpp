4cf29a81eb8a593a2bb27d145c18b3b8b96ca440
Israfil <smisrafil158@gmail.com>
2026-07-21 23:35:38 +0600

    Prompt line design changed.

--- src/terminal/pty.cpp
+++ src/terminal/pty.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <fcntl.h>
 #include <pty.h>
+#include <regex>
 #include <signal.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -50,9 +51,11 @@ bool PtySession::start() {
 
     if (pid == 0) {
         setenv("TERM", "xterm-256color", 1);
-        setenv("PS1", "\\u@\\h : \\w \\$ ", 1);
+        setenv("SHELL", shell_path_.c_str(), 1);
+        setenv("PS1", "\\[\\e[34m\\]\\u@\\h\\[\\e[0m\\] : \\[\\e[33m\\]\\w\\[\\e[0m\\] \\[\\e[31m\\]$(if [ \"$EUID\" -eq 0 ]; then echo '&'; else echo '\\$'; fi)\\[\\e[0m\\] ", 1);
+        setenv("PROMPT_COMMAND", "PS1='\\[\\e[34m\\]\\u@\\h\\[\\e[0m\\] : \\[\\e[33m\\]\\w\\[\\e[0m\\] \\[\\e[31m\\]$(if [ \"$EUID\" -eq 0 ]; then echo '&'; else echo '\\$'; fi)\\[\\e[0m\\] '", 1);
         setenv("PS2", "> ", 1);
-        setenv("PROMPT", "%n@%m : %~ %# ", 1);
+        setenv("PROMPT", "%F{blue}%n@%m%f : %F{yellow}%~%f %(!%F{red}&%f:%F{red}$%f) ", 1);
         setenv("LANG", "C.UTF-8", 1);
         setenv("LC_ALL", "C.UTF-8", 1);
         unsetenv("BASH_ENV");
@@ -60,13 +63,25 @@ bool PtySession::start() {
         const char* shell_name = strrchr(shell_path_.c_str(), '/');
         if (shell_name == nullptr) shell_name = shell_path_.c_str();
         else shell_name++;
-        execl(shell_path_.c_str(), shell_name, "-i", nullptr);
+        if (std::strcmp(shell_name, "bash") == 0) {
+            execl(shell_path_.c_str(), shell_name, "--noprofile", "--norc", "-i", nullptr);
+        } else if (std::strcmp(shell_name, "zsh") == 0) {
+            execl(shell_path_.c_str(), shell_name, "-f", "-i", nullptr);
+        } else {
+            execl(shell_path_.c_str(), shell_name, "-i", nullptr);
+        }
         _exit(127);
     }
 
     child_pid_ = pid;
     watch_id_ = g_unix_fd_add(master_fd_, static_cast<GIOCondition>(G_IO_IN | G_IO_ERR | G_IO_HUP), on_master_ready, this);
     return true;
+}
+
+std::string normalize_prompt_output(const std::string& text) {
+    static const std::regex prompt_re(R"((^|\n)(?:\x1B\[[0-9;]*m)*([^@\n]+)@([^:\n]+): ?([^\n#$]+?)([$#]) ?)");
+    static const std::string replacement = "$1\033[34m$2@$3\033[0m : \033[33m$4\033[0m \033[31m$5\033[0m ";
+    return std::regex_replace(text, prompt_re, replacement);
 }
 
 void PtySession::close() {
@@ -126,8 +141,10 @@ gboolean PtySession::on_master_ready(gint fd, GIOCondition condition, gpointer d
 }
 
 void PtySession::handle_output(const char* data, std::size_t size) {
+    std::string input(data, size);
+    input = normalize_prompt_output(input);
     if (output_handler_) {
-        output_handler_(std::string(data, size));
+        output_handler_(input);
     }
 }
 
